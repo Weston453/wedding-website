4f8f6aa4b8f2ba0aaa61c6bb84c980831c612598
Weston Isheim <wisheim@gmail.com>
2022-05-04 15:56:04 -0600

    fixed css after changing to link tag

--- src/components/NavBar/NavLinks.jsx
+++ src/components/NavBar/NavLinks.jsx
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom'
 const NavLinks = ({isMobile, closeMobileNav}) => {
   return (
     <nav>
+      <ul>
         <Link to="/">Home</Link>
         <Link to="/welcome">Welcome</Link>
         <Link to="/eventList">Events</Link>
         <Link to="/menuMain">Menu</Link>
         <Link to="/whosWho">Whos Who</Link>
         <Link to="/seating">Seating</Link>
+      </ul>
     </nav>
   )
 }
