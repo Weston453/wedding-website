b39fcd4e9b2a98e6dffd13959c1b4d5fb689269b
Weston Isheim <wisheim@gmail.com>
2022-05-18 11:27:10 -0600

    added na beer to open bar menu

--- src/components/Menu/open-bar/OpenBar.jsx
+++ src/components/Menu/open-bar/OpenBar.jsx
@@ -58,6 +58,7 @@ const OpenBar = () => {
             <p className={classes.SubMenuBeerPLast}>21st amendment - homeward bound</p>
             <h3 className={classes.SubMenuH3}>Bottles and Cans</h3>
             <p className={classes.SubMenuBeerP}>see bartender</p>
+            <p className={classes.SubMenuBeerP}>st. pauli girl - non-alcoholic</p>
           </div>
         </main>
       </div>
