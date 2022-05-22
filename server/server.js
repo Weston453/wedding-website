c93adccf109b3832ebd4dae34edf69cefb996d3b
Weston Isheim <wisheim@gmail.com>
2022-05-21 22:12:57 -0600

    replaced whole server file

--- server/server.js
+++ server/server.js
@@ -1,38 +1,18 @@
-import express from 'express'
-import fs from 'fs'
-import path from 'path'
-
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
-
-import App from '../src/App'
+import express from "express";
+import path from "path";
 
 const PORT = 8000;
 
 const app = express();
 
-app.use('^/$', (req, res, next) => {
-    const appcomponent = ReactDOMServer.renderToString(<App />);
-    fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data) => {
-        if(err) {
-            console.log(err)
-            return res.status(500).send("Some error occurred")
-        }
-        return res.send(
-            data.replace(
-                '<div id="root"></div>', 
-                `<div id="root">${appcomponent}</div>`
-            )
-        )
-    })
-})
 
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(express.static(path.resolve(__dirname, "..", "build")));
 
+//You were missing this
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..build/index.html"));
+  res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
