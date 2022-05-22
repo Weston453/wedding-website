285b46da139347cdc2a43528e95945a86dc98b10
Weston Isheim <wisheim@gmail.com>
2022-05-21 22:08:10 -0600

    added catch all end point to server

--- server/server.js
+++ server/server.js
@@ -7,16 +7,9 @@ import ReactDOMServer from 'react-dom/server'
 
 import App from '../src/App'
 
-// import { renderToString } from 'react-dom/server'
-// import Welcome from '../src/components/welcome/Welcome'
-// import EventList from '../src/components/events-list/EventList'
-// import MenuMain from '../src/components/Menu/Main-Menu/MenuMain'
-// import WhosWho from '../src/components/whos-who/WhosWho'
-// import Seating from '../src/components/seating/Seating'
+const PORT = 8000;
 
-const PORT = 8000
-
-const app = express()
+const app = express();
 
 app.use('^/$', (req, res, next) => {
     const appcomponent = ReactDOMServer.renderToString(<App />);
@@ -34,48 +27,12 @@ app.use('^/$', (req, res, next) => {
     })
 })
 
-app.use(express.static(path.resolve(__dirname, '..', 'build')))
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..build/index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
-
-// app.get('/', (req, res) => {
-//     const content = renderToString(<HeroPage />);
-
-//     res.send(content)
-// })
-
-// app.get('/welcome', (req, res) => {
-//     const content = renderToString(<Welcome />);
-
-//     res.send(content)
-// })
-
-// app.get('/eventList', (req, res) => {
-//     const content = renderToString(<EventList />);
-
-//     res.send(content)
-// })
-
-// app.get('/MenuMain', (req, res) => {
-//     const content = renderToString(<MenuMain />);
-
-//     res.send(content)
-// })
-
-// app.get('/whosWho', (req, res) => {
-//     const content = renderToString(<WhosWho />);
-
-//     res.send(content)
-// })
-
-// app.get('/seating', (req, res) => {
-//     const content = renderToString(<Seating />);
-
-//     res.send(content)
-// })
-
-// app.listen(3000, () => {
-//     console.log('listening on port 3000')
-// })
+    console.log(`Listening on port ${PORT}`);
+});
